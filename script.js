2c0d35f8da6bb2915b2ec6a4c0677c3af3952719
Emmanuel <ekasomo@corp.free.fr>
2023-11-25 20:17:40 +0100

    add form

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("formulaire");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const name = document.getElementById("inputName").value;
+    const email = document.getElementById("inputMail").value;
+    const message = document.getElementById("inputMessage").value;
+
+    alert(`${name} 
+          ${email}
+          ${message}`);
+  });
+});
